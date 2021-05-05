c9e56251e69a81a5a657c590d6b735ad9a70bedf
Himanshu Mishra <hm221311@gmail.com>
2021-05-05 16:55:55 +0530

    Deleted some files

---
+++ productData.js
@@ -0,0 +1,64 @@
+[
+  {
+    product_specific: {
+      product_sizes: ["S", "M", "L"],
+      product_colors: [],
+      product: [
+        {
+          _id: "608f0f058101ab200c85ad91",
+          color: "Black",
+          stocks: 10,
+          price: 1999,
+          size: "S",
+        },
+      ],
+    },
+    visible: true,
+    product_images_path: [
+      "456bda33-67c7-4952-9e88-c51f79c09992.jpg",
+      "281c073e-a9c1-457c-ac90-a1f62f9707ca.jpg",
+      "2c7be1df-9d5e-4053-a465-59a0f47be0af.jpg",
+    ],
+    product_available_for: ["male"],
+    _id: "608f0f058101ab200c85ad90",
+    product_name: "Running Shoes for Men",
+    product_description: "Black and Blue Running Shoes for men.From XYZ Brand",
+    product_wear_catagory: "Shoes",
+    createdAt: "2021-05-02T20:43:49.318Z",
+    updatedAt: "2021-05-02T20:43:49.318Z",
+    __v: 0,
+    product_brand: "Nike",
+  },
+  {
+    product_specific: {
+      product_sizes: ["S", "M", "L", "XL"],
+      product_colors: [],
+      product: [
+        {
+          _id: "608f119513380821be04acae",
+          color: "Brown",
+          stocks: 10,
+          price: 1399,
+          size: "S",
+        },
+      ],
+    },
+    visible: true,
+    product_images_path: [
+      "f3305542-b8cf-4d89-a854-41cb346eac28.jpg",
+      "cbc9070d-38cd-4358-954e-1739d4a2bcc2.jpg",
+      "fb11a742-d342-4823-a216-f36226dff807.jpg",
+      "154435ce-28db-424d-97fe-a10bb7c177dd.jpg",
+    ],
+    product_available_for: ["male"],
+    _id: "608f119513380821be04acad",
+    product_name: "Plain Shirt for Men",
+    product_brand: "US Polo",
+    product_description:
+      "This is a plain brown shirt. The build quality of this product is very good. It is made up of 100% cotton. Very comfortable to wear.",
+    product_wear_catagory: "Topwear",
+    createdAt: "2021-05-02T20:54:45.601Z",
+    updatedAt: "2021-05-02T20:54:45.601Z",
+    __v: 0,
+  },
+];
---
+++ public/Javascript/index.js
@@ -0,0 +1,19 @@
+const navslide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener("click", () => {
+
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            }
+            else { link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 +0.3}s` }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+navslide();
