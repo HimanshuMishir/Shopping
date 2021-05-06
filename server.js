3e6f112a48f3787dd7957c22be04dd3b6855b3d2
Himanshu Mishra <hm221311@gmail.com>
2021-05-06 07:18:28 +0530

    Server ready

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`The server is up and running on port ${process.env.PORT}`);
+});
